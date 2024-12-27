c685d5350b7e1695f16a07eafe9fe6db0a06a540
Aditya <kumar.aditya.2016585@gmail.com>
2024-12-27 14:34:33 +0530

    pass 1st stage

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -1,5 +1,5 @@
 import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.classic.methods.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.methods.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
