9413645e30d8808039bb168912b7f03e40ab7a3f
Aditya <kumar.aditya.2016585@gmail.com>
2024-12-28 13:50:33 +0530

    pass 1st stage

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -1,9 +1,7 @@
-import org.apache.hc.client5.http.classic.methods.CloseableHttpResponse;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import java.nio.charset.StandardCharsets;
 import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
@@ -27,23 +25,28 @@ public class HttpRequest{
             String peerId = Util.bytesToHex(peerIdBytes);    
 
 
-            HttpGet httpGet=new HttpGet(url);
-            httpGet.addHeader("info_hash",URLEncoder.encode(infoHash,StandardCharsets.UTF_8));
-            httpGet.addHeader("peer_id",URLEncoder.encode(peerId,StandardCharsets.UTF_8));
-            httpGet.addHeader("port", URLEncoder.encode("6881",StandardCharsets.UTF_8));
-            httpGet.addHeader("uploaded",URLEncoder.encode("0",StandardCharsets.UTF_8));
-            httpGet.addHeader("downloaded",URLEncoder.encode(infoHash,StandardCharsets.UTF_8));
-            httpGet.addHeader("left",URLEncoder.encode(String.valueOf(torrent.length),StandardCharsets.UTF_8));
-            httpGet.addHeader("compact",URLEncoder.encode("1",StandardCharsets.UTF_8));
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("info_hash",URLEncoder.encode(infoHash,StandardCharsets.UTF_8));
+                .addHeader("peer_id",URLEncoder.encode(peerId,StandardCharsets.UTF_8));
+                .addHeader("port", URLEncoder.encode("6881",StandardCharsets.UTF_8));
+                .addHeader("uploaded",URLEncoder.encode("0",StandardCharsets.UTF_8));
+                .addHeader("downloaded",URLEncoder.encode(infoHash,StandardCharsets.UTF_8));
+                .addHeader("left",URLEncoder.encode(String.valueOf(torrent.length),StandardCharsets.UTF_8));
+                .addHeader("compact",URLEncoder.encode("1",StandardCharsets.UTF_8));
+                .build();
 
             
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)){
-                if(response.getCode()!=200){
+            try (Response response = client.newCall(request).execute()){
+                if(response.isSuccessful()!){
                     System.out.println("Somethings Wrong");
                 }
                 else{
 
-                    byte[] responseBytes = EntityUtils.toByteArray(response.getEntity());
+                    byte[] responseBytes = response.body().bytes();
                     Bencode bencode=new Bencode(true);
                     Map<String ,Object> decodedResponse=bencode.decode(responseBytes,Type.DICTIONARY);
                     byte[] peersBytes=((ByteBuffer)decodedResponse.get("peers")).array();
