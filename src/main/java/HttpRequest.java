07191d0cd80615261f4b442254c1f37de15b21ba
Aditya <kumar.aditya.2016585@gmail.com>
2024-12-28 14:26:31 +0530

    pass 1st stage

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -33,7 +33,7 @@ public class HttpRequest{
                 .addHeader("info_hash",URLEncoder.encode(infoHash,StandardCharsets.ISO_8859_1))
                 .addHeader("peer_id",URLEncoder.encode(peerId,StandardCharsets.ISO_8859_1))
                 .addHeader("port", URLEncoder.encode("6881",StandardCharsets.ISO_8859_1))
-                .addHeader("uploaded",URLEncoder.encode("0",StandardCharsets.ISO_8859_1))
+                //.addHeader("uploaded",URLEncoder.encode("0",StandardCharsets.ISO_8859_1))
                 .addHeader("downloaded",URLEncoder.encode(infoHash,StandardCharsets.ISO_8859_1))
                 .addHeader("left",URLEncoder.encode(String.valueOf(torrent.length),StandardCharsets.ISO_8859_1))
                 .addHeader("compact",URLEncoder.encode("1",StandardCharsets.ISO_8859_1)) 
