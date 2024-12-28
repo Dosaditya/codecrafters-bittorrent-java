cb1ce0ec613e3d6b1d2b86fe51a1a755a4b60bd6
Aditya <kumar.aditya.2016585@gmail.com>
2024-12-28 13:54:00 +0530

    pass 1st stage

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -41,7 +41,7 @@ public class HttpRequest{
 
             
             try (Response response = client.newCall(request).execute()){
-                if(response.isSuccessful()!){
+                if(response.isSuccessful==false){
                     System.out.println("Somethings Wrong");
                 }
                 else{
