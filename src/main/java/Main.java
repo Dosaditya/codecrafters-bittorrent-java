0fb1538329a6a61f20237fd7baa3076e90fea796
Aditya <kumar.aditya.2016585@gmail.com>
2025-01-14 23:00:27 +0530

    pass 7st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
 
 
-        String command = args[0];
+        String command = args[0]; 
         if ("decode".equals(command)) {
             String bencodedValue = args[1];
             try {
